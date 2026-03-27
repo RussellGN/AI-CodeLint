f52389406a33cbde33ca86d5863a98cb90a5f7a1
russellgn <dudurussell@gmail.com>
2026-03-27 12:54:34 +0200

    fix: use full page range for diagnostics (temporary)

--- server/src/lsp.rs
+++ server/src/lsp.rs
@@ -130,10 +130,25 @@ impl Backend {
                 Ok(errs) => {
                     debug!("lint returned {} diagnostics for {}", errs.len(), doc_uri);
                     let diagnostics: Vec<Diagnostic> = errs.into_iter().map(|e| e.into()).collect();
+                    let text_lines_count: u32 = text
+                        .lines()
+                        .collect::<Vec<_>>()
+                        .len()
+                        .try_into()
+                        .expect("could not narrow usize into u32");
+                    let full_page_range =
+                        Range::new(Position::new(0, 0), Position::new(text_lines_count + 1, 0));
                     {
                         let mut docs = self.docs_being_watched.lock().await;
                         if let Some(doc) = docs.get_mut(doc_uri.as_str()) {
-                            doc.diagnostics = diagnostics.clone();
+                            doc.diagnostics = diagnostics
+                                .clone()
+                                .into_iter()
+                                .map(|mut d| {
+                                    d.range = full_page_range;
+                                    d
+                                })
+                                .collect();
                         } else {
                             warn!(
                                 "file disappeared from cache before diagnostics update: {}",
@@ -158,16 +173,25 @@ impl Backend {
 
 impl From<LintResult> for Diagnostic {
     fn from(value: LintResult) -> Self {
+        // let range = Range::new(
+        //     Position::new(value.start_line, 0),
+        //     Position {
+        //         line: value.end_line + 1,
+        //         character: 0,
+        //     },
+        // );
+        let placeholder_range = Range::new(
+            Position::new(0, 0),
+            Position {
+                line: 500,
+                character: 0,
+            },
+        );
+
         Self {
-            range: Range::new(
-                Position::new(value.start_line, 10000),
-                Position {
-                    line: value.end_line,
-                    character: 10_000,
-                },
-            ),
+            range: placeholder_range,
             message: value.overview,
-            severity: Some(DiagnosticSeverity::HINT),
+            severity: Some(DiagnosticSeverity::INFORMATION),
             source: Some(String::from("AI CodeLint")),
             tags: None,
             data: None,
