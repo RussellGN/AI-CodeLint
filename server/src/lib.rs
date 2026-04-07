4ffb531bf9d32d8f20f5e73c9c3c3e906a9594d3
russellgn <dudurussell@gmail.com>
2026-04-07 11:19:57 +0200

    refactor: package name retrieval

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -9,7 +9,7 @@ pub mod lsp;
 pub const OPENROUTER_BASE_URL: &str = "https://openrouter.ai/api/v1";
 pub const OPENROUTER_API_KEY: &str = include_str!("../.env");
 pub const DOCS_CACHE_SIZE: usize = 20;
-pub const CRATE_NAME: &str = "ai_codelint";
+pub const CRATE_NAME: &str = env!("CARGO_PKG_NAME");
 
 #[derive(Deserialize)]
 struct Status {
